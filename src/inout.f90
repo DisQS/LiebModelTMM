905fc5e71ed1f4f90312b3b5537c2f5731be4dd5
liujie <43362827+liujie940228@users.noreply.github.com>
2021-09-21 19:45:46 +0800

    Add files via upload
    !!$  OPEN(UNIT= IChInp, ERR= 120, FILE= "tmseLMxD.inp",STATUS= 'OLD')
      OPEN(UNIT= IChInp, ERR=120, FILE= "/dev/stdin",STATUS= 'OLD')
    
    Changed the standard input and output to suit for script file.

--- src/inout.f90
+++ src/inout.f90
@@ -93,8 +93,8 @@ SUBROUTINE Input( IErr )
   IErr = 0
   ILine= 0
   
-  OPEN(UNIT= IChInp, ERR= 120, FILE= "tmseLMxD.inp",&
-       STATUS= 'OLD')
+!!$  OPEN(UNIT= IChInp, ERR= 120, FILE= "tmseLMxD.inp",STATUS= 'OLD')
+  OPEN(UNIT= IChInp, ERR=120, FILE= "/dev/stdin",STATUS= 'OLD')
   
   ILine= ILine+1
   READ(IChInp,10,ERR=20,END=30) ISeed
